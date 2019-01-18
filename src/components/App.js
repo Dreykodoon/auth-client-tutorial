3f4a9d032fa6ce71d68432d2728a93d4d8d6ce5b
Mihai Cioban <ciobanmihai@gmail.com>
2019-01-18 16:10:03 +0200

    Added Header and welcome page

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Header from './Header';
 
-export default () => {
+
+export default ({children}) => {
     return (
         <div>
-            Hi, I'm the app!
+            <Header/>
+            {children}
         </div>
     );
 };
