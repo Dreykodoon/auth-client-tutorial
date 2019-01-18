f450449062ffe840ca87e8adcb505906bcf55c85
Mihai Cioban <ciobanmihai@gmail.com>
2019-01-18 16:16:16 +0200

    Added Sign Up page placeholder

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import {BrowserRouter, Route} from 'react-router-dom';
 
 import App from './components/App';
 import Welcome from './components/Welcome';
+import SignUp from './components/auth/SignUp';
 
 
 ReactDOM.render(
     <BrowserRouter>
         <App>
             <Route exact path="/" component={Welcome}/>
+            <Route exact path="/signup" component={SignUp}/>
         </App>
     </BrowserRouter>,
     document.querySelector('#root')
