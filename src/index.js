08076f5833719f05ce3624d329e1d67e4a042b9d
Mihai Cioban <ciobanmihai@gmail.com>
2019-01-18 16:25:45 +0200

    Added redux

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 
+import reducers from './reducers';
 import App from './components/App';
 import Welcome from './components/Welcome';
 import SignUp from './components/auth/SignUp';
 
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App>
-            <Route exact path="/" component={Welcome}/>
-            <Route exact path="/signup" component={SignUp}/>
-        </App>
-    </BrowserRouter>,
+    <Provider store={createStore(reducers, {})}>
+        <BrowserRouter>
+            <App>
+                <Route exact path="/" component={Welcome}/>
+                <Route exact path="/signup" component={SignUp}/>
+            </App>
+        </BrowserRouter>
+    </Provider>,
     document.querySelector('#root')
 );
---
+++ src/reducers/auth.js
@@ -0,0 +1,8 @@
+const INIT_STATE = {
+    authenticated: '',
+    errorMessage: ''
+};
+
+export default function(state = INIT_STATE, action) {
+    return state;
+}
